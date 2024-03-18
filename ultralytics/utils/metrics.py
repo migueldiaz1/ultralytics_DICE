85be3e58cd817a74513322be2100f53f12b75867
migueldiaz1 <87194086+migueldiaz1@users.noreply.github.com>
2024-03-18 18:17:47 +0100

    Update metrics.py

--- ultralytics/utils/metrics.py
+++ ultralytics/utils/metrics.py
@@ -150,6 +150,24 @@ def mask_iou(mask1, mask2, eps=1e-7):
     return intersection / (union + eps)
 
 
+def mask_iou(mask1, mask2, eps=1e-7):
+    """
+    Calculate masks IoU.
+
+    Args:
+        mask1 (torch.Tensor): A tensor of shape (N, n) where N is the number of ground truth objects and n is the
+                        product of image width and height.
+        mask2 (torch.Tensor): A tensor of shape (M, n) where M is the number of predicted objects and n is the
+                        product of image width and height.
+        eps (float, optional): A small value to avoid division by zero. Defaults to 1e-7.
+
+    Returns:
+        (torch.Tensor): A tensor of shape (N, M) representing masks IoU.
+    """
+    intersection = torch.matmul(mask1, mask2.T).clamp_(0)
+    union = (mask1.sum(1)[:, None] + mask2.sum(1)[None]) - intersection  # (area1 + area2) - intersection
+    return 2*intersection / (union + intersection + eps)
+
 def kpt_iou(kpt1, kpt2, area, sigma, eps=1e-7):
     """
     Calculate Object Keypoint Similarity (OKS).
